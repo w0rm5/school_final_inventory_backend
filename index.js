e31e627236c5d87be2e4af052de4a82c836e2ca6
w0rm5 <47029568+w0rm5@users.noreply.github.com>
2021-10-12 16:55:03 +0700

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose"
+import cors from "cors"
 import "dotenv/config";
 
 import { login } from "./controllers/user.js";
@@ -15,6 +16,10 @@ import stock_out from "./routers/stock_out.js";
 const app = express();
 const port = process.env.PORT || process.env.API_PORT;
 const dbConString = process.env.DB;
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200
+}
 
 // const options = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, autoIndex: true, }
 
@@ -24,6 +29,7 @@ mongoose.connect(dbConString, (err) => {
         return
     }
 
+    app.use(cors(corsOptions))
     app.use(express.json());
 
     // app.get('/', verifyToken, checkIfAdmin, (req, res) => { //testing
